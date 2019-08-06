dbd70716d56fe2bec0ab6890167f55ca8d5f1336
AMOREM <amorem@amorem.com>
2019-08-06 11:04:16 +0200

    Fonts and rounded corners

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Shop',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato '),
       home: ProductsOverviewScreen(),
     );
   }
