f517a891bd4904aa2a63d7cb7867843a4dc17563
AMOREM <amorem@amorem.com>
2019-08-10 14:57:29 +0200

    Stateful to Stateless using FutureBuilder

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -5,46 +5,37 @@ import '../providers/orders.dart';
 import '../widgets/app_drawer.dart';
 import '../widgets/order_item.dart' as widg;
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   static const routeName = '/orders';
 
   @override
-  _OrdersScreenState createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  var isLoading = false;
-
-  @override
-  void initState() {
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        isLoading = true;
-      });
-      await Provider.of<Orders>(context, listen: false).fetchOrders();
-      setState(() {
-        isLoading = false;
-      });
-    });
-
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final ordersData = Provider.of<Orders>(context);
+    // final ordersData = Provider.of<Orders>(context);
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Your orders'),
-      ),
-      drawer: AppDrawer(),
-      body: isLoading
-          ? Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemCount: ordersData.orders.length,
-              itemBuilder: (ctx, index) =>
-                  widg.OrderItem(ordersData.orders[index]),
-            ),
-    );
+        appBar: AppBar(
+          title: Text('Your orders'),
+        ),
+        drawer: AppDrawer(),
+        body: FutureBuilder(
+          future: Provider.of<Orders>(context, listen: false).fetchOrders(),
+          builder: ((ctx, data) {
+            if (data.connectionState == ConnectionState.active) {
+              return Center(child: CircularProgressIndicator());
+            } else {
+              if (data.error != null) {
+                return Center(
+                  child: Text('An error occured'),
+                );
+              } else {
+                return Consumer<Orders>(
+                    builder: (ctx, orderData, child) => ListView.builder(
+                          itemCount: orderData.orders.length,
+                          itemBuilder: (ctx, index) =>
+                              widg.OrderItem(orderData.orders[index]),
+                        ));
+              }
+            }
+          }),
+        ));
   }
 }
