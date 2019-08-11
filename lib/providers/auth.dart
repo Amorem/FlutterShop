4ee8b80249ea13aeff9f863978623cdc9e2c6083
AMOREM <amorem@amorem.com>
2019-08-11 13:18:42 +0200

    add logout in Drawer

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -70,4 +70,11 @@ class Auth with ChangeNotifier {
       throw error;
     }
   }
+
+  Future<void> logout() {
+    _token = null;
+    _userId = null;
+    _expiryDate = null;
+    notifyListeners();
+  }
 }
--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../providers/auth.dart';
+import 'package:provider/provider.dart';
 
 import '../screens/orders_screen.dart';
 import '../screens/user_products_screen.dart';
@@ -37,7 +39,15 @@ class AppDrawer extends StatelessWidget {
             Navigator.of(context)
                 .pushReplacementNamed(UserProductsScreen.routeName);
           },
-        )
+        ),
+        Divider(),
+        ListTile(
+          leading: Icon(Icons.exit_to_app),
+          title: Text('Logout'),
+          onTap: () {
+            Provider.of<Auth>(context, listen: false).logout();
+          },
+        ),
       ],
     ));
   }
