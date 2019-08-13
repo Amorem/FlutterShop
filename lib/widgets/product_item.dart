0e509ad72185a6fe251c900136696e0ac9892f8e
AMOREM <amorem@amorem.com>
2019-08-13 13:13:15 +0200

    Hero animation (easy !)

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -22,12 +22,15 @@ class ProductItem extends StatelessWidget {
             Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
                 arguments: product.id);
           },
-          child: FadeInImage(
-            placeholder: AssetImage('assets/images/product-placeholder.png'),
-            image: NetworkImage(
-              product.imageUrl,
+          child: Hero(
+            tag: product.id,
+            child: FadeInImage(
+              placeholder: AssetImage('assets/images/product-placeholder.png'),
+              image: NetworkImage(
+                product.imageUrl,
+              ),
+              fit: BoxFit.cover,
             ),
-            fit: BoxFit.cover,
           ),
         ),
         footer: GridTileBar(
